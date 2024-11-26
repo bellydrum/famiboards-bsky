85bf54f93924d8a0d066a251675fb15e4432383d
David Maness <davidmaness1990@gmail.com>
2024-11-25 20:54:17 -0600

    update logging

--- utils.ts
+++ utils.ts
@@ -18,7 +18,7 @@ export async function getNewThreads() {
         return thread.text && !storedThreadTitles.includes(thread.text)
     })
 
-    await writeStoredThreads(latestThreads)
+    if (newThreads.length) await writeStoredThreads(latestThreads)
 
     return newThreads
 }
